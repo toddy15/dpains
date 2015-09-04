530cc8fc5eb399a4a895802d4c71af3f1df2bd99
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-09-04 14:28:04 +0200

    Add model and table for episodes

---
+++ database/migrations/2015_09_04_122017_create_episodes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number');
+            $table->string('name');
+            $table->string('start_date');
+            $table->integer('job_id')->references('id')->on('jobs');;
+            $table->decimal('vk', 4, 3);
+            $table->decimal('factor_night', 4, 3);
+            $table->decimal('factor_nef', 4, 3);
+            $table->integer('comment_id')->references('id')->on('comments');;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('episodes');
+    }
+}
