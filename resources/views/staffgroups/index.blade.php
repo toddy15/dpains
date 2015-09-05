8e05ebecc9c38107a422d0d16916ce173e1407b0
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-09-05 07:20:28 +0200

    Add heading and create functionality

--- resources/views/staffgroups/index.blade.php
+++ resources/views/staffgroups/index.blade.php
@@ -1,6 +1,10 @@
 @extends('app')
 
 @section('content')
+    <h1>Mitarbeitergruppen</h1>
+    <p>
+        <a class="btn btn-primary" href="{{ action('StaffgroupController@create') }}">Neue Mitarbeitergruppe erstellen</a>
+    </p>
     <table class="table table-striped">
         <thead>
             <th>Mitarbeitergruppe</th>
@@ -12,7 +16,7 @@
                 <tr>
                     <td>{{ $staffgroup->staffgroup }}</td>
                     <td>{{ $staffgroup->weight }}</td>
-                    <td><a class="btn btn-primary btn-default" href="{{ action('StaffgroupController@edit', $staffgroup->id) }}">Bearbeiten</a></td>
+                    <td><a class="btn btn-primary" href="{{ action('StaffgroupController@edit', $staffgroup->id) }}">Bearbeiten</a></td>
                 </tr>
             @endforeach
         </tbody>
