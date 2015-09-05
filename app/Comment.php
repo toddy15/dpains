414d13ee4b75082da89de633a449cf7bb50cdf06
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-09-05 07:25:00 +0200

    Add model and table for comment

---
+++ app/Comment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'comment'
+    ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
