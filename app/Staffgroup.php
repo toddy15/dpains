d313c3a25c08b6b16028e8d4ddaa0c359ce8d360
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-09-05 06:32:34 +0200

    Do not use timestamps for model Staffgroup

--- app/Staffgroup.php
+++ app/Staffgroup.php
@@ -9,4 +9,11 @@ class Staffgroup extends Model
     protected $fillable = [
         'staffgroup', 'weight'
     ];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
