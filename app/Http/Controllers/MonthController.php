c588612bab621dd2cbac2206773e33a13e7d03f0
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-09-07 12:57:42 +0200

    Add query and subquery for people working in a given month

--- app/Http/Controllers/MonthController.php
+++ app/Http/Controllers/MonthController.php
@@ -6,11 +6,62 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class MonthController extends Controller
 {
-    public function show($month)
+    /**
+     * Show the people working in the given month with their
+     * calculated night shifts and nef shifts.
+     *
+     * @param $year
+     * @param $month
+     * @return mixed
+     */
+    public function show($year, $month)
     {
-        return $month;
+        // Ensure valid values for year and month
+        $year = (int) $year;
+        $month = (int) $month;
+        if (($year < 2014) or ($month < 1) or ($month > 12)) {
+            abort(404);
+        }
+        // Convert to internal representation in the database (YYYY-MM)
+        $formatted_month = sprintf("%4d-%02d", $year, $month);
+        $staff = $this->getPeopleForMonth($formatted_month);
+        return $staff;
+    }
+
+    /**
+     * Returns an array of people working in the given month.
+     *
+     * @param $month
+     * @return mixed
+     */
+    private function getPeopleForMonth($month)
+    {
+        return DB::table('episodes as e1')
+            ->leftJoin('staffgroups', 'e1.staffgroup_id', '=', 'staffgroups.id')
+            ->leftJoin('comments', 'e1.comment_id', '=', 'comments.id')
+            // With this complicated subquery we get the row with the
+            // current data for the specified month.
+            ->where('e1.start_date', function ($query) use ($month) {
+                $query->selectRaw('MAX(`e2`.`start_date`)')
+                    ->from('episodes as e2')
+                    ->whereRaw('`e1`.`number` = `e2`.`number`')
+                    ->where('e2.start_date', '<=', $month);
+            })
+            // This filters out the episodes with "Vertragsende".
+            // In order to get episodes without a comment (= NULL)
+            // as well, we need to include those comments explicitely.
+            ->where(function ($query) {
+                $query->where('comment', 'not like', 'Vertragsende')
+                    ->orWhereNull('comment');
+            })
+            // First, order by staffgroups (weight parameter)
+            ->orderBy('weight')
+            // Second, order by name within the staffgroups
+            ->orderBy('name')
+            ->get();
     }
 }
