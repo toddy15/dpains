1d6bcdd2e142e69174c5a9e6719c3c3a7722ef8f
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-09-12 18:00:52 +0200

    Extend plan parser

--- app/Dpains/Planparser.php
+++ app/Dpains/Planparser.php
@@ -2,10 +2,23 @@
 
 namespace App\Dpains;
 
+use Illuminate\Support\Facades\DB;
+
 class Planparser
 {
+    public $month = '';
     public $names = [];
     public $shifts = [];
+
+    /**
+     * Planparser constructor.
+     * @param string $month
+     */
+    public function __construct($month)
+    {
+        $this->month = $month;
+    }
+
 
     public function parseNames($people)
     {
@@ -72,5 +85,67 @@ class Planparser
         } elseif ($lines_per_person == 3) {
             $this->shifts = $work;
         }
+    }
+
+    public function storeShiftsForPerson()
+    {
+        // Clean all previously parsed results.
+        DB::table('analyzed_months')->where('month', $this->month)->delete();
+        // Get people names for this episode.
+        $reporter = new Reporter();
+        // Get an array with the unique person's number and name
+        $expected_names = $reporter->getNamesForMonth($this->month);
+        $database_rows = [];
+        foreach ($this->names as $id => $name) {
+            $person_number = array_search($name, $expected_names);
+            $shifts = $this->calculateShifts($this->shifts[$id]);
+            if (!is_array($shifts)) {
+                drupal_set_message(t('Plan data not saved.'), 'error');
+                // Clean all previously parsed results.
+                db_delete('ains_parsed_months')
+                    ->condition('month', $month)
+                    ->execute();
+                return;
+            }
+            $database_rows[] = [
+                'month' => $this->month,
+                'number' => $person_number,
+                'nights' => $shifts['nights'],
+                'nefs' => $shifts['nefs'],
+            ];
+        }
+        DB::table('analyzed_months')->insert($database_rows);
+        // @TODO: FEEDBACK
+    }
+
+    public function calculateShifts($shifts)
+    {
+        // @TODO: Do not hardcode.
+        $nights = ['0r', 'D0', 'D1', 'i30', 'i36', 'n2'];
+        $nefs = ['n1', 'n2'];
+        $ignored = ['1', '2', 'st', '25', '26', '27', 'D2', 'i28', 'i29', 'i33', 'i35', 'dt0', 'dt1',
+            'FÜ', 'BF', 'TZ', 'MS', 'EZ', 'U', 'FBi*', 'FBe*', 'Con', 'K', 'KO', 'Kol', 'KP', 'KK',
+            'KÜ', 'ZU', 'BR', 'BU', 'F.', '', 'DB', 'Ve', 'US', '--', 'BV'
+        ];
+        $night_counter = 0;
+        $nef_counter = 0;
+        foreach ($shifts as $shift) {
+            $unknown_shift = TRUE;
+            if (in_array($shift, $nights)) {
+                $night_counter++;
+                $unknown_shift = FALSE;
+            }
+            if (in_array($shift, $nefs)) {
+                $nef_counter++;
+                $unknown_shift = FALSE;
+            }
+            if (in_array($shift, $ignored)) {
+                $unknown_shift = FALSE;
+            }
+            if ($unknown_shift) {
+                return 'Unbekannte Dienstart: ' . $shift;
+            }
+        }
+        return ['nights' => $night_counter, 'nefs' => $nef_counter];
     }
 }
--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -14,12 +14,11 @@ class ReportController extends Controller
 {
     public function index()
     {
-        $planparser = new Planparser();
+        $planparser = new Planparser('2015-01');
         $rawplan = Rawplan::where('month', '2015-01')->first();
         $planparser->parseNames($rawplan->people);
         $planparser->parseShifts($rawplan->shifts);
-        var_dump($planparser->names);
-        var_dump($planparser->shifts);
+        $planparser->storeShiftsForPerson();
     }
 
     public function show($year, $month)
