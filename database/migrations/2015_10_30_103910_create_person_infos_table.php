46fbed074baf4e06c8464407184a87d208e30c7e
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-10-30 10:54:12 +0100

    Add PersonInfo model and migration

---
+++ database/migrations/2015_10_30_103910_create_person_infos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person_infos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('number')->unique();
+            $table->string('email', 64)->unique();
+            $table->string('hash', 64);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('person_infos');
+    }
+}
