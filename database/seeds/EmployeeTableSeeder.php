34c19060b7636e095860c4ad173abb021673fa12
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-11-14 17:24:18 +0100

    Enable seeding again

--- database/seeds/EmployeeTableSeeder.php
+++ database/seeds/EmployeeTableSeeder.php
@@ -12,7 +12,7 @@ class EmployeeTableSeeder extends Seeder
     public function run()
     {
         // Find out the highest number currently in use
-        $highest_person_number = DB::table('episodes')->max('number');
+        $highest_person_number = DB::table('episodes')->max('employee_id');
         // Insert blank entries for all ids currently in use.
         $time = date('Y-m-d H:i:s');
         for ($id = 1; $id <= $highest_person_number; $id++) {
