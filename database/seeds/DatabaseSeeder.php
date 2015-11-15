5e0297a58e7b7f676e4a41a59db858d979801ef6
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-11-15 15:47:09 +0100

    Create a user upon installation

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $this->call(UserTableSeeder::class);
         $this->call(StaffgroupTableSeeder::class);
         $this->call(CommentTableSeeder::class);
         $this->call(EmployeeTableSeeder::class);
