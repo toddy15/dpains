36d23c2905c35e3a849af8fd465627a8f42d3436
Dr. Tobias Quathamer <t.quathamer@mailbox.org>
2015-11-18 21:57:46 +0100

    Fix syntax error in URL generation

--- app/Http/Controllers/AnonController.php
+++ app/Http/Controllers/AnonController.php
@@ -116,7 +116,7 @@ class AnonController extends Controller
         $employee->save();
         // Send the mail
         // @TODO: Do not hardcode
-        $url = action('AnonController@showYear', 2016, $employee->hash);
+        $url = action('AnonController@showYear', [2016, $employee->hash]);
         Mail::queue(['text' => 'emails.new_hash'], compact('url'), function ($m) use ($employee) {
             $m->to($employee->email);
             $m->subject('Neuer Zugriffscode für www.dienstplan-an.de');
