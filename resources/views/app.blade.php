da9d3e07c0e4032b42f7fc6f349461ab00c5903e
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-03 13:18:55 +0200

    Update app template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,29 +1,33 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="de">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Dienstplan AN AKA</title>
-        <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
-    </head>
-    <body>
-        @include('partials.nav')
-        <div class="container">
-            @if ($errors->any())
-                <div class="alert alert-danger alert-dismissible" role="alert">
-                    <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            @include('partials.info')
-            @yield('content')
-        </div>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="{{ asset(mix('js/app.js')) }}"></script>
-    </body>
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset(mix('css/app.css')) }}" rel="stylesheet">
+
+    <title>Dienstplan AN AKA</title>
+</head>
+<body>
+@include('partials.nav')
+<div class="container">
+    @if ($errors->any())
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span
+                    class="sr-only">Close</span></button>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @include('partials.info')
+    @yield('content')
+</div>
+
+<script src="{{ asset(mix('js/app.js')) }}"></script>
+</body>
 </html>
