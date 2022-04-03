7620c0113fe82688f4a51488c588f6ab4d2de813
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-03 13:11:17 +0200

    Remove custom auth() routes

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Authentication routes...
-Route::get('auth/login', 'App\Http\Controllers\Auth\AuthController@getLogin');
-Route::post('auth/login', 'App\Http\Controllers\Auth\AuthController@postLogin');
-Route::get('auth/logout', 'App\Http\Controllers\Auth\AuthController@getLogout');
-
-// Registration routes...
-//Route::get('auth/register', 'App\Http\Controllers\Auth\AuthController@getRegister');
-//Route::post('auth/register', 'App\Http\Controllers\Auth\AuthController@postRegister');
-
-// Password reset link request routes...
-Route::get('password/email', 'App\Http\Controllers\Auth\PasswordController@getEmail');
-Route::post('password/email', 'App\Http\Controllers\Auth\PasswordController@postEmail');
-Route::get('password/reset/{token}', 'App\Http\Controllers\Auth\PasswordController@getReset');
-Route::post('password/reset', 'App\Http\Controllers\Auth\PasswordController@postReset');
-
 /*
  * These routes are only accessible by authenticated users.
  */
