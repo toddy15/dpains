ea6c0dcab0ed2d5ca3b514eefa667b98b588dbff
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-03 21:43:02 +0200

    Remove HomeController

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::middleware([Authenticate::class])->group(function () {
     Route::resource('episode', EpisodeController::class)
         ->except(['index', 'show']);
