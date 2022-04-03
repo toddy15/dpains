f283f45b4b450ff62f640a90ba763aa8cdf53f17
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-03 13:54:32 +0200

    Update alerts

--- resources/views/partials/info.blade.php
+++ resources/views/partials/info.blade.php
@@ -1,20 +1,20 @@
 @if (Session::has('danger'))
-    <div class="alert alert-danger alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Schließen</span></button>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Schließen"></button>
         {{ Session::get('danger') }}
     </div>
 @endif
 
 @if (Session::has('warning'))
-    <div class="alert alert-warning alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Schließen</span></button>
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Schließen"></button>
         {{ Session::get('warning') }}
     </div>
 @endif
 
 @if (Session::has('info'))
-    <div class="alert alert-info alert-dismissible" role="alert">
-        <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Schließen</span></button>
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Schließen"></button>
         {{ Session::get('info') }}
     </div>
 @endif
