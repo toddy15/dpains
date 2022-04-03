6dcfaec4e1419b868d4402bd5583e5a6f0fa50f2
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-03 21:22:10 +0200

    Some small view updates

--- resources/views/rawplans/create.blade.php
+++ resources/views/rawplans/create.blade.php
@@ -6,12 +6,14 @@
     {!! Form::open(['action' => 'App\Http\Controllers\RawplanController@store']) !!}
 
     <!-- Month Form Input  -->
-    <div class="form-group">
-        {!! Form::label('month', 'Monat:', ['class' => 'form-label']) !!}
-        <div class="form-inline">
-            {!! Form::selectMonth('month', $selected_month, ['class' => 'form-control']) !!}
-            {!! Form::label('year', 'Jahr:', ['class' => 'sr-only form-label']) !!}
-            {!! Form::selectYear('year', $start_year, $end_year, $selected_year, ['class' => 'form-control']) !!}
+    <div class="row">
+        {!! Form::label('month', 'Monat:', ['class' => 'col-sm-4 col-form-label']) !!}
+        <div class="col-sm-4">
+            {!! Form::selectMonth('month', $selected_month, ['class' => 'form-select']) !!}
+        </div>
+        {!! Form::label('year', 'Jahr:', ['class' => 'visually-hidden col-sm-4 col-form-label']) !!}
+        <div class="col-sm-4">
+            {!! Form::selectYear('year', $start_year, $end_year, $selected_year, ['class' => 'form-select']) !!}
         </div>
     </div>
 
@@ -19,25 +21,19 @@
     <div class="form-group {{ $errors->has('people') ? 'has-error has-feedback' : '' }}">
         {!! Form::label('people', 'Mitarbeiter:', ['class' => 'form-label']) !!}
         {!! Form::textarea('people', null, ['class' => 'form-control']) !!}
-        @if ($errors->has('people'))
-            <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-        @endif
     </div>
 
     <!-- Shifts Form Input  -->
     <div class="form-group {{ $errors->has('shifts') ? 'has-error has-feedback' : '' }}">
         {!! Form::label('shifts', 'Schichten:', ['class' => 'form-label']) !!}
         {!! Form::textarea('shifts', null, ['class' => 'form-control']) !!}
-        @if ($errors->has('shifts'))
-            <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-        @endif
     </div>
 
     <div class="form-group text-center">
         <!-- Speichern Form Input  -->
-        {!! Form::submit('Speichern', ['class' => 'btn btn-primary']) !!}
-        <!-- Cancel Button -->
-        <a class="btn btn-default" href="{{ action('App\Http\Controllers\RawplanController@index') }}">Abbrechen</a>
+    {!! Form::submit('Speichern', ['class' => 'btn btn-primary']) !!}
+    <!-- Cancel Button -->
+        <a class="btn btn-secondary" href="{{ action('App\Http\Controllers\RawplanController@index') }}">Abbrechen</a>
     </div>
 
     {!! Form::close() !!}
