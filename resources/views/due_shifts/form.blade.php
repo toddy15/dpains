bb112618512fb77d509dd58d698e6973cc6118ff
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-05 21:17:18 +0200

    Use components instead of Form helper

--- resources/views/due_shifts/form.blade.php
+++ resources/views/due_shifts/form.blade.php
@@ -1,42 +1,24 @@
-<!-- Year Form Input  -->
-<div class="form-group {{ $errors->has('year') ? 'has-error has-feedback' : '' }}">
-    {!! Form::label('year', 'Jahr:', ['class' => 'form-label']) !!}
-    {!! Form::text('year', null, ['class' => 'form-control']) !!}
-    @if ($errors->has('year'))
-        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-    @endif
-</div>
+<x-label for="year" value="Jahr:"/>
+<x-input name="year" id="year"
+         value="{{ old('year', $due_shift->year ?? '') }}"
+         invalid="{{ $errors->has('year') }}"/>
 
-<!-- Staffgroup Form Input  -->
-<div class="form-group {{ $errors->has('staffgroup_id') ? 'has-error has-feedback' : '' }}">
-    {!! Form::label('staffgroup_id', 'Mitarbeitergruppe:', ['class' => 'form-label']) !!}
-    {!! Form::select('staffgroup_id', $staffgroups, null, ['class' => 'form-select']) !!}
-    @if ($errors->has('staffgroup_id'))
-        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-    @endif
-</div>
+<x-label for="staffgroup_id" value="Mitarbeitergruppe:"/>
+{!! Form::select('staffgroup_id', $staffgroups, null, ['class' => 'form-select']) !!}
 
-<!-- Nights Form Input  -->
-<div class="form-group {{ $errors->has('nights') ? 'has-error has-feedback' : '' }}">
-    {!! Form::label('nights', 'Nächte:', ['class' => 'form-label']) !!}
-    {!! Form::text('nights', null, ['class' => 'form-control']) !!}
-    @if ($errors->has('nights'))
-        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-    @endif
-</div>
+<x-label for="nights" value="Nächte:"/>
+<x-input name="nights" id="nights"
+         type="number"
+         value="{{ old('nights', $due_shift->nights ?? '') }}"
+         invalid="{{ $errors->has('nights') }}"/>
 
-<!-- Nefs Form Input  -->
-<div class="form-group {{ $errors->has('nefs') ? 'has-error has-feedback' : '' }}">
-    {!! Form::label('nefs', 'NEF-Schichten:', ['class' => 'form-label']) !!}
-    {!! Form::text('nefs', null, ['class' => 'form-control']) !!}
-    @if ($errors->has('nefs'))
-        <span class="glyphicon glyphicon-remove form-control-feedback"></span>
-    @endif
-</div>
+<x-label for="nefs" value="NEF-Schichten:"/>
+<x-input name="nefs" id="nefs"
+         type="number"
+         value="{{ old('nefs', $due_shift->nefs ?? '') }}"
+         invalid="{{ $errors->has('nefs') }}"/>
 
 <div class="form-group text-center mt-4">
-    <!-- Speichern Form Input  -->
-    {!! Form::submit('Speichern', ['class' => 'btn btn-primary']) !!}
-    <!-- Cancel Button -->
-    <a class="btn btn-secondary" href="{{ $cancel_url }}">Abbrechen</a>
+    <x-button>Speichern</x-button>
+    <x-link-button href="{{ route('due_shift.index') }}" class="btn-secondary">Abbrechen</x-link-button>
 </div>
