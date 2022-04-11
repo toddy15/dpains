37e26d9e7396120efa1cb228d8629df8a63dfe34
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-11 23:00:47 +0200

    Translate logout

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -88,7 +88,7 @@
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
+                            Abmelden
                         </a>
 
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
