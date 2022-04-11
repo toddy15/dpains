d687d94dcfdf5fcef224af4d2a41a4bff0a85116
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-11 21:47:43 +0200

    Add test for rawplans

---
+++ tests/Feature/RawplanTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+test('a guest cannot view the rawplans', function () {
+    get(route('rawplans.index'))->assertRedirect(route('login'));
+});
+
+test('a user can view the rawplans', function () {
+    actingAs(User::factory()->create());
+    get(route('rawplans.index'))
+        ->assertOk()
+        ->assertViewIs('rawplans.index')
+        ->assertViewHas('rawplans_planned');
+});
+
+test('a user can create a rawplan');
