1b1d49dc7d9477be75ed8f50321310a014294369
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-11 21:36:31 +0200

    Test submitting a new comment

--- tests/Feature/CommentsTest.php
+++ tests/Feature/CommentsTest.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Models\Comment;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
 test('a guest cannot view the comments', function () {
     get(route('comments.index'))->assertRedirect(route('login'));
@@ -14,4 +16,17 @@ test('a user can view the comments', function () {
         ->assertOk()
         ->assertViewIs('comments.index')
         ->assertViewHas('comments');
+});
+
+test('a user can create a comment', function () {
+    actingAs(User::factory()->create());
+
+    $comment = Comment::factory()->raw();
+    get(route('comments.index'))->assertDontSeeText($comment['comment']);
+
+    post(route('comments.store', $comment))->assertRedirect(
+        route('comments.index'),
+    );
+
+    get(route('comments.index'))->assertSeeText($comment['comment']);
 });
