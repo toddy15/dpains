37ac0d4b7871c55255c595345cfdaff8b3d4d264
Dr. Tobias Quathamer <toddy@debian.org>
2022-04-15 16:41:50 +0200

    Revert "FIX: Don't use TrimStrings middleware on people and shifts"
    This reverts commit 2986c502bb0c250244aadcb62de89d33eec742ed.

--- app/Http/Middleware/TrimStrings.php
+++ app/Http/Middleware/TrimStrings.php
@@ -15,8 +15,5 @@ class TrimStrings extends Middleware
         'current_password',
         'password',
         'password_confirmation',
-        // @TODO: Remove this for rawplans
-        'people',
-        'shifts',
     ];
 }
