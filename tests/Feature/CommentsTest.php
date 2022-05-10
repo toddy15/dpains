029de6eb5ff964494674e8ab84004717d213f04b
Dr. Tobias Quathamer <toddy@debian.org>
2022-05-10 14:21:26 +0200

    Add a test

--- tests/Feature/CommentsTest.php
+++ tests/Feature/CommentsTest.php
@@ -18,11 +18,19 @@ test('a user can view the comments', function () {
         ->assertViewHas('comments');
 });
 
+test('a guest cannot create a comment', function () {
+    $comment = Comment::factory()->raw();
+
+    post(route('comments.store', $comment))->assertRedirect(route('login'));
+});
+
 test('a user can create a comment', function () {
     actingAs(User::factory()->create());
 
     $comment = Comment::factory()->raw();
     get(route('comments.index'))->assertDontSeeText($comment['comment']);
+
+    get(route('comments.create'))->assertSeeText('Bemerkung:');
 
     post(route('comments.store', $comment))->assertRedirect(
         route('comments.index'),
