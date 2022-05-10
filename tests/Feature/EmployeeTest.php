5b5ec3db3ede26ff907948cbbc3248d0c26bb1b3
Dr. Tobias Quathamer <toddy@debian.org>
2022-05-10 14:22:45 +0200

    Add new test for employees

---
+++ tests/Feature/EmployeeTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Employee;
+use App\Models\Episode;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+use function Pest\Laravel\put;
+
+test('a user can access pages', function () {
+    actingAs(User::factory()->create());
+    $episode = Episode::factory()->create();
+    $employee = Employee::find($episode->employee_id);
+
+    get(route('employees.index'))->assertOk();
+    get(route('past.index'))->assertOk();
+    get(route('employees.edit', $employee))->assertOk();
+    //    put(route('employees.update', $employee))->assertOk();
+});
+
+/*
+  PUT|PATCH       employees/{employee} .............................................. employees.update › EmployeeController@update
+  GET|HEAD        employees/month/{year}/{month} .................................................... EmployeeController@showMonth
+  GET|HEAD        employees/vk/{which_vk}/{year} ................................................ EmployeeController@showVKForYear
+  GET|HEAD        employees/{id}/episodes ........................................................ EmployeeController@showEpisodes
+ */
