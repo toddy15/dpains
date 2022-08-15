1bceb158228a7aeea8c09df6c9ceee23f1de8e1f
Dr. Tobias Quathamer <toddy@debian.org>
2022-08-15 23:01:31 +0200

    Update rector config for this project

--- rector.php
+++ rector.php
@@ -2,20 +2,32 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src'
+        __DIR__.'/app',
+        __DIR__.'/database',
+        __DIR__.'/resources',
+        __DIR__.'/routes/web.php',
+        __DIR__.'/tests',
+    ]);
+
+    $rectorConfig->skip([
+        __DIR__.'/database/factories/UserFactory.php',
+        __DIR__.'/tests/Pest.php',
+        __DIR__.'/app/Console',
+        __DIR__.'/app/Exceptions',
+        __DIR__.'/app/Http/Middleware',
+        __DIR__.'/app/Providers',
     ]);
 
     // register a single rule
-    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+    $rectorConfig->rule(CompactToVariablesRector::class);
 
     // define sets of rules
-    //    $rectorConfig->sets([
-    //        LevelSetList::UP_TO_PHP_81
-    //    ]);
+//    $rectorConfig->sets([
+//        LevelSetList::UP_TO_PHP_81,
+//    ]);
 };
