7686a4039025ef7d6ab7ab7dbaea20b475f19f38
Dr. Tobias Quathamer <toddy@debian.org>
2022-08-16 22:52:13 +0200

    Add new test for episodes

--- tests/Feature/EmployeeTest.php
+++ tests/Feature/EmployeeTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\EmployeeController;
 use App\Models\Employee;
 use App\Models\Episode;
---
+++ tests/Feature/EpisodeTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\Employee;
+use App\Models\Episode;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+use function Pest\Laravel\post;
+
+test('a user can create a new episode for a new employee', function () {
+    actingAs(User::factory()->create());
+    // This creates already an employee, so the next employee has ID 2.
+    $episode = Episode::factory()->make();
+    $data = $episode->toArray();
+    $data['employee_id'] = 0;
+    $data['month'] = $episode->month;
+    $data['year'] = $episode->year;
+
+    post(route('episodes.store', $data))
+        ->assertRedirect(route('employees.episodes.index', ['employee' => 2]));
+});
+
+test('a user can create a new episode for an existing employee', function () {
+    actingAs(User::factory()->create());
+    $episode_1 = Episode::factory()->create();
+    $episode_2 = Episode::factory()->make();
+    $data = $episode_2->toArray();
+    $data['employee_id'] = $episode_1->employee_id;
+    $data['month'] = $episode_2->month;
+    $data['year'] = $episode_2->year;
+
+    // Ensure that both names are different
+    expect($episode_1->name)->not()->toBe($episode_2->name);
+
+    // There should be only one episode with a name
+    get(route('employees.episodes.index', ['employee' => $episode_1->employee_id]))
+        ->assertSeeText($episode_1->name)
+        ->assertDontSeeText($episode_2->name);
+
+    post(route('episodes.store', $data))
+        ->assertRedirect(route('employees.episodes.index', ['employee' => $episode_1->id]));
+
+    // Now there should be two episodes with a name
+    get(route('employees.episodes.index', ['employee' => $episode_1->id]))
+        ->assertSeeText($episode_1->name)
+        ->assertSeeText($episode_2->name);
+});
