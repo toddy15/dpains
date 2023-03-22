91ac61d6a22d4656422ebe26b55e72db9488a07c
Dr. Tobias Quathamer <toddy@debian.org>
2023-03-22 15:26:01 +0100

    Increase test coverage

--- tests/Feature/CommentsTest.php
+++ tests/Feature/CommentsTest.php
@@ -3,8 +3,11 @@
 use App\Models\Comment;
 use App\Models\User;
 use function Pest\Laravel\actingAs;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Laravel\assertDatabaseMissing;
 use function Pest\Laravel\get;
 use function Pest\Laravel\post;
+use function Pest\Laravel\put;
 
 test('a guest cannot view the comments', function () {
     get(route('comments.index'))->assertRedirect(route('login'));
@@ -38,3 +41,37 @@ test('a user can create a comment', function () {
 
     get(route('comments.index'))->assertSeeText($comment['comment']);
 });
+
+test('a guest cannot edit a comment', function () {
+    $comment = Comment::factory()->create();
+    get(route('comments.edit', $comment->id))->assertRedirect(route('login'));
+});
+
+test('a user can edit a comment', function () {
+    $comment = Comment::factory()->create();
+    actingAs(User::factory()->create());
+    get(route('comments.edit', $comment->id))
+        ->assertOk()
+        ->assertViewIs('comments.edit')
+        ->assertViewHas('comment')
+        ->assertSee($comment->comment);
+});
+
+test('a guest cannot update a comment', function () {
+    $comment = Comment::factory()->create();
+    put(route('comments.update', $comment->id), [
+        'comment' => 'This is a new comment.',
+    ])->assertRedirect(route('login'));
+});
+
+test('a user can update a comment', function () {
+    $comment = Comment::factory()->create();
+    assertDatabaseMissing('comments', ['comment' => 'This is a new comment.']);
+
+    actingAs(User::factory()->create());
+    put(route('comments.update', $comment->id), [
+        'comment' => 'This is a new comment.',
+    ])->assertRedirect(route('comments.index'));
+
+    assertDatabaseHas('comments', ['comment' => 'This is a new comment.']);
+})->only();
