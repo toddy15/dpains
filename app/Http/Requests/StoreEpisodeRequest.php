6b2ae86c44e5906c7583b5f3357e6e4ad55c637d
Dr. Tobias Quathamer <toddy@debian.org>
2023-11-02 17:01:40 +0100

    Reformat

--- app/Http/Requests/StoreEpisodeRequest.php
+++ app/Http/Requests/StoreEpisodeRequest.php
@@ -24,30 +24,12 @@ class StoreEpisodeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => [
-                'required',
-            ],
-            'month' => [
-                'required',
-            ],
-            'year' => [
-                'required',
-            ],
-            'vk' => [
-                'required',
-                'numeric',
-                'between:0,1',
-            ],
-            'factor_night' => [
-                'required',
-                'numeric',
-                'between:0,2',
-            ],
-            'factor_nef' => [
-                'required',
-                'numeric',
-                'between:0,2',
-            ],
+            'name' => ['required'],
+            'month' => ['required'],
+            'year' => ['required'],
+            'vk' => ['required', 'numeric', 'between:0,1'],
+            'factor_night' => ['required', 'numeric', 'between:0,2'],
+            'factor_nef' => ['required', 'numeric', 'between:0,2'],
         ];
     }
 }
