094d4fbe839f2b772bda54acf356f3b08b1f72f0
Dr. Tobias Quathamer <toddy@debian.org>
2024-02-27 22:42:14 +0100

    Convert rector.php to new syntax

--- rector.php
+++ rector.php
@@ -3,18 +3,17 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__.'/app',
         __DIR__.'/database',
         __DIR__.'/resources',
         __DIR__.'/routes/web.php',
         __DIR__.'/tests',
-    ]);
-
-    $rectorConfig->skip([
+    ])
+    ->withRootFiles()
+    ->withSkip([
         __DIR__.'/database/factories/UserFactory.php',
         __DIR__.'/tests/Pest.php',
         __DIR__.'/app/Console',
@@ -22,13 +21,3 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__.'/app/Http/Middleware',
         __DIR__.'/app/Providers',
     ]);
-
-    // register a single rule
-    $rectorConfig->rule(NullToStrictStringFuncCallArgRector::class);
-
-    // define sets of rules
-    $rectorConfig->sets([
-        //        LevelSetList::UP_TO_PHP_82,
-        //        LaravelSetList::LARAVEL_90,
-    ]);
-};
