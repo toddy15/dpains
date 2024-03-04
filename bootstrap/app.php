be3035c1d1fdc0dcb9031ace47643f218de038f8
Shift <shift@laravelshift.com>
2024-03-04 08:40:18 +0000

    Re-register service providers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,9 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders()
+    ->withProviders([
+        \App\Providers\AppServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         // api: __DIR__.'/../routes/api.php',
