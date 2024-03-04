35721145a0094cfd3dbd266e4fa40ace6d38b308
Shift <shift@laravelshift.com>
2024-03-04 08:40:17 +0000

    Disable updating vendor migration dates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        \Illuminate\Foundation\Console\VendorPublishCommand::dontUpdateMigrationDates();
     }
 }
