cee66876dca8745a031d8e8d658047d812d3ffba
Dr. Tobias Quathamer <toddy@debian.org>
2024-09-08 15:26:31 +0200

    Remove onclick attribute for CSP

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+
+// Enable CSP and remove inline script for logout menu item
+document.addEventListener('DOMContentLoaded', function() {
+    const logoutLink = document.getElementById('logout-link');
+    logoutLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        document.getElementById('logout-form').submit();
+    });
+});
