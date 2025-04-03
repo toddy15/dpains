eab1f73f52f35208b79f01a858a991fa59392875
Dr. Tobias Quathamer <toddy@debian.org>
2025-04-03 20:14:34 +0200

    Add test for BDs

--- tests/Feature/ReportTest.php
+++ tests/Feature/ReportTest.php
@@ -49,3 +49,18 @@ it('calculates the number of nightshifts', function () {
 
     expect($response->content())->toMatchSnapshot();
 });
+
+it('calculates the number of BDs', function () {
+    $this->seed(EpisodesSeeder::class);
+    loadDataset('2025-01_nightshifts');
+
+    // Provide a constant username for snapshot testing
+    actingAs(User::factory(['name' => 'Testuser'])->create());
+
+    $response = $this->get('/report/bd/2025');
+    $response->assertOk()
+        ->assertSee('Bereitschaftsdienste 2025')
+        ->assertDontSee('Seite nicht vorhanden');
+
+    expect($response->content())->toMatchSnapshot();
+});
