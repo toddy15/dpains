148fb0d46673a41841e29678c154c8ee53074818
Dr. Tobias Quathamer <toddy@debian.org>
2025-07-07 14:35:21 +0200

    Increase type coverage

--- rector.php
+++ rector.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\SetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -22,4 +21,5 @@ return RectorConfig::configure()
         __DIR__.'/app/Http/Middleware',
         __DIR__.'/app/Providers',
     ])
-    ->withPreparedSets(deadCode: true);
+    ->withPreparedSets(deadCode: true)
+    ->withTypeCoverageLevel(0);
