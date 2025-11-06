c138fa8ac516ea8bed95070219abbcd06d65670d
Dr. Tobias Quathamer <toddy@debian.org>
2025-11-06 21:39:33 +0100

    Fix PHPStan issue and prevent negative integers

--- app/Http/Controllers/EpisodeController.php
+++ app/Http/Controllers/EpisodeController.php
@@ -34,6 +34,7 @@ class EpisodeController extends Controller
             // There are no episodes, so create a new employee
             // using sane default values.
             $wb_id = Staffgroup::where('staffgroup', 'WB')->firstOrFail()->id;
+            $wb_id = max(0, $wb_id);
             $episode = new Episode;
             $episode->start_date = Carbon::now()->isoFormat('YYYY-MM');
             $episode->staffgroup_id = $wb_id;
